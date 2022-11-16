808659f1aded18a7a2b78266a5052167a53a0d78
STS665-VIMALA S <vimalasivashanmugam1@gmail.com>
2022-11-16 19:15:17 +0530

    Done changes

--- JobApplication.Services/JobServices.cs
+++ JobApplication.Services/JobServices.cs
@@ -11,22 +11,36 @@ namespace JobApplication.Services
 {
     public  class JobServices:IJobService
     {
+        #region Declaration
         private readonly IJobRepository jobRepository;
+        #endregion
+
+        #region Constructor
         public JobServices(IJobRepository jobRepository)
         {
             this.jobRepository = jobRepository;
         }
+        #endregion
+
+        #region Creating new applicant detail
         public void CreateForm(JobModel jobModel)
         {
             jobRepository.CreateForm(jobModel);
         }
+        #endregion
+
+        #region Listing the applicant details
         public List<JobModel> ListDetail()
         {
             return jobRepository.ListDetail();
         }
+        #endregion
+
+        #region Viewing details in partial view from database
         public JobModel Save(int ID)
         {
             return jobRepository.Save(ID);
         }
+        #endregion
     }
 }
